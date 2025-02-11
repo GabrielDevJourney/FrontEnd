4953c9226d03714e5c15cbc0aeabd04825ccabb7
Gabriel <gabspereira178@gmail.com>
2025-02-11 11:46:56 +0000

    implemeted need script for creating and populated deck cotnainer in visually in the page with both type of cards templates

--- cardWarGame/src/script.js
+++ cardWarGame/src/script.js
@@ -1,6 +1,31 @@
-const deck = document.querySelector(".deck-container");
-const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
+const deck = document.querySelector(".deckContainer");
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A"];
+const faces = ["J", "Q", "K"];
 const suits = ["hearts", "diamonds", "clubs", "spades"];
+
+//create cards of hight ranks
+function createFaceCard(face, suit) {
+	return `
+        <div class="card" data-value="${face}" data-suit="${suit}">
+            <div class="cardInfo">
+                <div class="info">
+                    <div class="infoNumber">${face}</div>
+                    <div class="infoSymbol"><img src="./assets/${suit}.png"></div>
+                </div>
+            </div>
+            <div class="imageContainer">
+                <img src="./assets/${face}_${suit}.png">
+                <img src="./assets/${face}_${suit}.png">
+            </div>
+            <div class="cardInfoFliped">
+                <div class="info">
+                    <div class="infoNumber">${face}</div>
+                    <div class="infoSymbol"><img src="./assets/${suit}.png"></div>
+                </div>
+            </div>
+        </div>
+    `;
+}
 
 function createCard(value, suit) {
 	return `
@@ -30,16 +55,34 @@ function createCard(value, suit) {
             </div>
             <div class="cardInfoFliped">
                 <div class="info">
-                    <div class="infoNumber">${value}</div>
-                    <div class="infoSymbol"><img src="./assets/${suit}.png"></div>
+                <div class="infoSymbol"><img src="./assets/${suit}.png"></div>
+                <div class="infoNumber">${value}</div>
                 </div>
             </div>
         </div>
     `;
 }
 
-values.forEach((value) => {
-	suits.forEach((suit) => {
-		deck.innerHTML += createCard(value, suit);
+function generateNumberCards() {
+	values.forEach((value) => {
+		suits.forEach((suit) => {
+			deck.innerHTML += createCard(value, suit);
+		});
 	});
-});
+}
+
+function generateFaceCards() {
+	faces.forEach((face) => {
+		suits.forEach((suit) => {
+			deck.innerHTML += createFaceCard(face, suit);
+		});
+	});
+}
+
+function createDeck() {
+	deck.innerHTML = "";
+	generateNumberCards();
+	generateFaceCards();
+}
+
+createDeck();
